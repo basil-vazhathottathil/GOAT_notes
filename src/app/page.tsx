db234b5101e8783ae934b3d8709f26f554434754
Basil Vazhathottathil <basilveldose@gmail.com>
2025-03-20 12:32:27 +0530

    header add buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Homepage() {
   return (
-    <div className='bg-blue-400'>home</div>
+    <h3>blaah</h3>
   )
 }
 
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { shadow } from './utils'
+import { Button } from './button';
+
+const Header = () => {
+    const user=null;
+  return (
+    <header className='relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8'
+    style={
+        {
+            boxShadow :shadow
+        }
+    }>
+        <Link href='/' className='flex items-end gap-2'>
+            <Image src='/goat.png' alt='Goat Logo' width={50} height={50} priority className='rounded-full' />
+            <h1 className='flex flex-col pb-1 text-2xl font-semibold leading-6'>
+            GOAT <span>Notes</span>
+        </h1>
+        </Link>
+
+        <div className='flex gap-4'>
+            {
+                user ? (
+                    // <Button onClick={() => {}}>LogOut</Button>
+                    "logout"
+                ) : (
+                    <>
+                        <Button asChild variant="outline">
+                            <Link href="/login">Login</Link>
+                        </Button>
+                        <Button asChild className='hidden sm:block'>
+                            <Link href="/signup">Signup</Link>
+                        </Button>
+                    </>
+                )
+            }
+        </div>
+    </header>
+  )
+}
+
+export default Header
